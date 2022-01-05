a29abd3bef9df07d78d4e3c8b7bba106c819831e
Hugo Colin <colin.hugo.89@gmail.com>
2022-01-04 19:34:18 -0600

    Se corrigio el archivo control.controller.js para busquedas por y sin fecha

--- controllers/control.controller.js
+++ controllers/control.controller.js
@@ -2,14 +2,43 @@ const { Control } = require( '../models' );
 
 const obtenerReportes = async ( req, res ) => {
 
-    const { desde, hasta } = req.body;
+    try {
+
+        const reporte = await Control.find();
+
+        if ( reporte.length === 0 ) {
+
+            return res.json( {
+                value: 0,
+                msg: 'No hay movimientos que mostrar.'
+            } );
+        }
+
+        return res.json( {
+            value: 1,
+            reporte
+        } );
+        
+    } catch ( error ) {
+
+        console.error( 'Error al mostrar los movimientos realizados por los usuarios.', error )
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al mostrar los movimientos realizados por los usuarios.'
+        } );
+    }
+}
+
+const obtenerReportesFecha = async ( req, res ) => {
+
+    const { desde } = req.params;
 
     try {
 
         const reporte = await Control.find( {
             createdAt: {
-                $gte: desde,
-                $lte: hasta
+                $gte: desde
             }
         } );
 
@@ -38,5 +67,6 @@ const obtenerReportes = async ( req, res ) => {
 }
 
 module.exports = {
-    obtenerReportes
+    obtenerReportes,
+    obtenerReportesFecha
 }
