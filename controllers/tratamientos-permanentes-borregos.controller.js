22efd393e0146fb8cbfb5531a8af5796aaedba00
Hugo Colin <colin.hugo.89@gmail.com>
2022-02-17 15:52:54 -0600

    Se cambio el nombre de tratamientos a tratamientos permanentes

---
+++ controllers/tratamientos-permanentes-borregos.controller.js
@@ -0,0 +1,139 @@
+const { Borrego, TratamientoPermanenteBorrego } = require( '../models' );
+
+const { generarControl } = require( '../helpers/generar-control' );
+
+const obtenerTratamientosBorregos = async ( req, res ) => {
+
+    try {
+
+        const tratamientos = await TratamientoPermanenteBorrego.find()
+            .populate( 'borrego', 'numeroBorrego' );
+
+        if ( tratamientos.length === 0 ) {
+            return res.json( {
+                value: 0,
+                msg: 'No hay tratamientos registrados.'
+            } );
+        }
+
+        return res.json( {
+            value: 1,
+            tratamientos
+        } );
+        
+    } catch ( error ) {
+
+        console.error( 'Error al obtener los tratamientos permanentes.' );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al obtener los tratamientos permanentes.'
+        } );
+    }
+}
+
+const obtenerTratamientoBorregoById = async ( req, res ) => {
+
+    const { idBorrego } = req.params;
+
+    try {
+
+        const tratamiento = await TratamientoPermanenteBorrego.where( { borrego: idBorrego } )
+            .populate( 'borrego', 'numeroBorrego' );
+
+        if ( tratamiento.length === 0 ) {
+            return res.json( {
+                value: 0,
+                msg: 'No hay tratamiento registrado.'
+            } )
+        }
+
+        return res.json( {
+            value: 1,
+            tratamiento
+        } );
+        
+    } catch ( error ) {
+
+        console.error( 'Error al obtener el tratamiento del borrego.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al obtener el tratamiento del borrego.'
+        } );
+    }
+
+}
+
+const registrarTratamientoBorrego = async ( req, res ) => {
+
+    const { nombre, apellidos } = req.body.usuario;
+    const { idBorrego } = req.params;
+
+    try {
+
+        const borrego = await Borrego.findById( idBorrego );
+
+        req.body.borrego = borrego;
+
+        const tratamiento = new TratamientoPermanenteBorrego( req.body );
+
+        await tratamiento.save();
+
+        generarControl( nombre, apellidos, 'registrado un tratamiento permanente al borrego número', borrego.numeroBorrego );
+
+        return res.json( {
+            value: 1,
+            msg: 'El tratamiento se ha registrado.',
+            tratamiento
+        } );
+        
+    } catch ( error ) {
+
+        console.error( 'Error al registrar tratamiento permanente.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al registrar tratamiento permanente.'
+        } );
+    }
+}
+
+const actualizarTratamientoBorrego = async ( req, res ) => {
+
+    const { nombre, apellidos } = req.body.usuario;
+
+    const { idTratamiento } = req.params;
+    const { ...datos } = req.body;
+
+    try {
+
+        const tratamiento = await TratamientoPermanenteBorrego.findByIdAndUpdate( idTratamiento, datos, { new: true } );
+
+        const borrego = await Borrego.findById( tratamiento.borrego );
+
+        generarControl( nombre, apellidos, 'actualizado un tratamiento permanente al borrego número', borrego.numeroBorrego );
+        
+        return res.json( {
+            value: 1,
+            msg: 'El tratamiento permanente se ha actualizado.',
+            tratamiento
+        } );
+
+    } catch ( error ) {
+
+        console.error( 'Error al actualizar el tratamiento permanente.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al actualizar el tratamiento permanente.'
+        } );
+    }
+}
+
+module.exports = {
+    obtenerTratamientosBorregos,
+    obtenerTratamientoBorregoById,
+    registrarTratamientoBorrego,
+    actualizarTratamientoBorrego
+}
---
+++ routes/tratamientos-permanentes.routes.js
@@ -0,0 +1,108 @@
+const router = require( 'express' ).Router();
+const { check } = require( 'express-validator' );
+
+const { validarCampos, validarJWT } = require( '../middlewares' );
+
+const { existeBorrego, existeTratamientoBorrego, 
+        existeCaballo, existeTratamiento, 
+        existeMascota, existeTratamientoMascota } = require( '../helpers/db-validators' );
+
+const { obtenerTratamientosBorregos, obtenerTratamientoBorregoById,
+        registrarTratamientoBorrego, 
+        actualizarTratamientoBorrego } = require( '../controllers/tratamientos-permanentes-borregos.controller' );
+
+const { obtenerTratamientosCaballos, obtenerTratamientoCaballoById,
+        registrarTratamientoCaballo, 
+        actualizarTratamientoCaballo } = require( '../controllers/tratamientos-permanentes-caballos.controller' );
+
+const { obtenerTratamientosMascotas, obtenerTratamientoMascotaById,
+        registrarTratamientoMascota, 
+        actualizarTratamientoMascota } = require( '../controllers/tratamientos-permanentes-mascotas.controller' );
+
+// ****************************************************
+// - End points para los tratamientos de los caballos -
+// ****************************************************
+
+router.get( '/caballos', obtenerTratamientosCaballos );
+
+router.get( '/caballos/:idCaballo', [
+    check( 'idCaballo', 'No es un id válido' ).isMongoId(),
+    check( 'idCaballo' ).custom( existeCaballo ),
+    validarCampos
+], obtenerTratamientoCaballoById );
+
+router.post( '/caballos/:idCaballo', [
+    validarJWT,
+    check( 'idCaballo', 'No es un id válido' ).isMongoId(),
+    check( 'idCaballo' ).custom( existeCaballo ),
+    check( 'tratamiento', 'El tratamiento es obligatorio.' ).escape().trim().notEmpty(),
+    check( 'descripcion', 'La descripción es obligatoria.' ).escape().trim().notEmpty(),
+    validarCampos
+], registrarTratamientoCaballo );
+
+router.put( '/caballos/:idTratamiento', [
+    validarJWT,
+    check( 'idTratamiento', 'No es un id válido' ).isMongoId(),
+    check( 'idTratamiento' ).custom( existeTratamiento ),
+    validarCampos
+], actualizarTratamientoCaballo );
+
+// ****************************************************
+// - End points para los tratamientos de los borregos -
+// ****************************************************
+
+router.get( '/borregos', obtenerTratamientosBorregos );
+
+router.get( '/borregos/:idBorrego', [
+    check( 'idBorrego', 'No es un id válido' ).isMongoId(),
+    check( 'idBorrego' ).custom( existeBorrego ),
+    validarCampos
+], obtenerTratamientoBorregoById );
+
+router.post( '/borregos/:idBorrego', [
+    validarJWT,
+    check( 'idBorrego', 'No es un id válido' ).isMongoId(),
+    check( 'idBorrego' ).custom( existeBorrego ),
+    check( 'tipo', 'El tipo de tratamiento es obligatorio.' ).escape().trim().notEmpty(),
+    check( 'descripcion', 'La descripción del tratamiento es obligatoria.' ).escape().trim().notEmpty(),
+    check( 'video', 'El link del video es obligatorio.' ).escape().trim().notEmpty(),
+    validarCampos
+], registrarTratamientoBorrego );
+
+router.put( '/borregos/:idTratamiento', [
+    validarJWT,
+    check( 'idTratamiento', 'No es un id válido' ).isMongoId(),
+    check( 'idTratamiento' ).custom( existeTratamientoBorrego ),
+    validarCampos
+], actualizarTratamientoBorrego );
+
+// ****************************************************
+// - End points para los tratamientos de las mascotas -
+// ****************************************************
+
+router.get( '/mascotas', obtenerTratamientosMascotas );
+
+router.get( '/mascotas/:idMascota', [
+    check( 'idMascota', 'No es un id válido' ).isMongoId(),
+    check( 'idMascota' ).custom( existeMascota ),
+    validarCampos
+], obtenerTratamientoMascotaById );
+
+router.post( '/mascotas/:idMascota', [
+    validarJWT,
+    check( 'idMascota', 'No es un id válido' ).isMongoId(),
+    check( 'idMascota' ).custom( existeMascota ),
+    check( 'tipo', 'El tipo de tratamiento es obligatorio.' ).escape().trim().notEmpty(),
+    check( 'descripcion', 'La descripción del tratamiento es obligatoria.' ).escape().trim().notEmpty(),
+    check( 'video', 'El link del video es obligatorio.' ).escape().trim().notEmpty(),
+    validarCampos
+], registrarTratamientoMascota );
+
+router.put( '/mascotas/:idTratamiento', [
+    validarJWT,
+    check( 'idTratamiento', 'No es un id válido' ).isMongoId(),
+    check( 'idTratamiento' ).custom( existeTratamientoMascota ),
+    validarCampos
+], actualizarTratamientoMascota );
+
+module.exports = router;
